e0b858027cf5fde6e05a87b89dd308b17baa87ca
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-27 11:17:23 +0530

    feat(profilepage.js): add search and create icon buttons

--- components/ProfilePage/index.js
+++ components/ProfilePage/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 import styles from "./styles.module.css";
+import IconButtonsGroup from "../UI/IconButtonsGroup";
+import IconButton from "../UI/IconButton";
+import { FcSearch, FcPlus } from "react-icons/fc";
+import { FaPlus } from "react-icons/fa";
 
 const ProfilePage = () => {
   return (
@@ -11,6 +15,17 @@ const ProfilePage = () => {
       </div>
       <div className={styles.mainContainer}>
         <Main />
+      </div>
+
+      <div className={styles.iconButtonsGroupContainer}>
+        <IconButtonsGroup>
+          <IconButton>
+            <FcSearch />
+          </IconButton>
+          <IconButton>
+            <FaPlus />
+          </IconButton>
+        </IconButtonsGroup>
       </div>
     </div>
   );
