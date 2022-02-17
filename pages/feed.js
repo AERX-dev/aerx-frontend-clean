7487c1394707c3c8cf6e5b840fbc38d9e0b16fa9
Sam Ullman <samullman@gmail.com>
2022-02-17 11:07:35 -0600

    feed responsive

--- pages/feed.js
+++ pages/feed.js
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
 
+import {
+	Box
+} from "@chakra-ui/react";
+
 function Feed () {
 	return (
 		<Layout>
+			<Box className="px-4 md:px-10" py={4}>
 			<h1>Feed</h1>
+			</Box>
 		</Layout>
 	);
 }
