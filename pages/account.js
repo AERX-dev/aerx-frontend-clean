eb6410f31cf5cfab81533b31e4745d4a87b8ee27
Sam Ullman <samullman@gmail.com>
2022-02-19 14:24:53 -0600

    chore: update account

--- pages/account.js
+++ pages/account.js
@@ -11,6 +11,8 @@ import {
   Button,
   InputGroup,
   InputLeftElement,
+  Image as ChakraImage,
+  Grid,
 } from "@chakra-ui/react";
 import { AtSignIcon } from "@chakra-ui/icons";
 import supabase from '../lib/supabase';
@@ -80,7 +82,6 @@ const Page = () => {
     let newProfile = profile;
     newProfile.headerImage = info.cdnUrl;
     setProfile(newProfile);
-    document.querySelectorAll(".header-picture")[0].value = info.cdnUrl;
   }
 
   function update(e) {
@@ -99,12 +100,29 @@ const Page = () => {
       .update(profile)
       .match({ walletId: profileId })
 
-      if (!error && data.length) {
-        setProfile(data[0]);
-      } else {
-        console.log(error)
-      }
+    if (!error && data.length) {
+      setProfile(data[0]);
+    } else {
+      console.log(error)
+    }
   }
+
+  function headerImage() {
+
+    if (profile.headerImage) {
+      return <Box height="220px" rounded="lg" overflow="hidden" mb={3}>
+        <ChakraImage src={profile.headerImage} alt="header" width="100%" objectFit="cover" height="100%" />
+      </Box>
+    }
+
+  }
+
+  function profileImage() {
+    if (profile.profileImage) {
+      return <ChakraImage height="320px" rounded="lg" maxWidth={["100%", "400px", "225px"]} margin="0 auto" src={profile.profileImage} alt="header" objectFit="cover" />
+    }
+  }
+
   return (
     <Layout>
 
@@ -114,7 +132,7 @@ const Page = () => {
 
         <FormControl mb={2}>
           <FormLabel>{t('label.headerPicture')}</FormLabel>
-          <Input className="header-picture" type="text" defaultValue={ profile.headerImage }  mb={2} placeholder="headerPicture" />
+          {headerImage()}
 
           <Button size="sm" onClick={() => { widgetApiHeader.current.openDialog() }} colorScheme={"teal"} mb={2}>
             {t('label.upload')}
@@ -127,21 +145,37 @@ const Page = () => {
 
         </FormControl>
 
-        <FormControl mb={2}>
-          <FormLabel>{t('label.profilePicture')}</FormLabel>
-          <Input className="profile-picture" type="text" defaultValue={ profile.profileImage }  mb={2} placeholder="profilePicture" />
 
-          <Button size="sm" colorScheme={"blue"} mb={2} onClick={() => { widgetApi.current.openDialog() }}>
-            {t('label.upload')}
-          </Button>
+        <Grid templateColumns={["repeat(100%)", "repeat(100%)", "220px calc(100% - 200px)"]} gap="20px">
+          <Box  overflow={"hidden"} rounded="lg" maxWidth={["100%", "400px", "225px"]}  margin="0 auto">
 
-          <Box height={0} width={0} opacity={0}>
-            <Widget ref={widgetApi} onChange={profileImageChange}
-              publicKey='9a62ac3cb175e8d52479' id='file' />
+            <FormControl mb={2}  >
+              <FormLabel>{t('label.profilePicture')}</FormLabel>
+              <Box height="320px" mb={2}>
+               {profileImage()}
+              
+              </Box>
+
+              <Button size="sm" colorScheme={"blue"} mb={2} onClick={() => { widgetApi.current.openDialog() }}>
+                {t('label.upload')}
+              </Button>
+
+              <Box height={0} width={0} opacity={0}>
+                <Widget ref={widgetApi} onChange={profileImageChange}
+                  publicKey='9a62ac3cb175e8d52479' id='file' />
+              </Box>
+            </FormControl>
+
           </Box>
+
+          <Box pr={8}>
+
+          <FormControl mb={2}>
+          <FormLabel>{t('label.fullName')}</FormLabel>
+          <Input type="email" defaultValue={profile.fullName} placeholder="fullName" onChange={update} data-path="fullName" />
         </FormControl>
 
-        <FormControl mb={2}>
+          <FormControl mb={2}>
           <FormLabel>{t('label.username')}</FormLabel>
           <InputGroup>
             <InputLeftElement
@@ -150,38 +184,35 @@ const Page = () => {
             >
               <AtSignIcon color='gray.300' />
             </InputLeftElement>
-            <Input placeholder="username" defaultValue={ profile.username } type="text" onChange={update} data-path="username" />
+            <Input placeholder="username" defaultValue={profile.username} type="text" onChange={update} data-path="username" />
           </InputGroup>
         </FormControl>
 
         <FormControl mb={2}>
           <FormLabel>{t('label.email')}</FormLabel>
-          <Input type="text" defaultValue={ profile.email } placeholder="email" onChange={update} data-path="email" />
+          <Input type="text" defaultValue={profile.email} placeholder="email" onChange={update} data-path="email" />
         </FormControl>
 
-        <FormControl mb={2}>
-          <FormLabel>{t('label.fullName')}</FormLabel>
-          <Input type="email" defaultValue={ profile.fullName } placeholder="fullName" onChange={update} data-path="fullName" />
-        </FormControl>
+       
 
         <FormControl mb={2}>
           <FormLabel>{t('label.aboutMe')}</FormLabel>
-          <Textarea type="email" defaultValue={ profile.aboutMe }  placeholder="aboutMe" onChange={update} data-path="aboutMe" />
+          <Textarea type="email" defaultValue={profile.aboutMe} placeholder="aboutMe" onChange={update} data-path="aboutMe" />
         </FormControl>
 
         <FormControl mb={2}>
           <FormLabel>{t('label.city')}</FormLabel>
-          <Input placeholder="city" defaultValue={ profile.city }  onChange={update} data-path="city" />
+          <Input placeholder="city" defaultValue={profile.city} onChange={update} data-path="city" />
         </FormControl>
 
         <FormControl mb={2}>
           <FormLabel>{t('label.state')}</FormLabel>
-          <Input placeholder="State/Province" defaultValue={ profile.state }  onChange={update} data-path="state" />
+          <Input placeholder="State/Province" defaultValue={profile.state} onChange={update} data-path="state" />
         </FormControl>
 
         <FormControl mb={2}>
           <FormLabel>{t('label.country')}</FormLabel>
-          <Input placeholder="Country" defaultValue={ profile.country }  onChange={update} data-path="country" />
+          <Input placeholder="Country" defaultValue={profile.country} onChange={update} data-path="country" />
         </FormControl>
 
 
@@ -190,6 +221,12 @@ const Page = () => {
         <Button colorScheme="green" mt={2} size="lg" onClick={save}>
           {t('label.save')}
         </Button>
+          </Box>
+        </Grid>
+
+
+
+       
       </Box>
     </Layout>
   )
