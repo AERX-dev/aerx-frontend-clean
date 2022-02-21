ae754a02c6aeb89d5b44ee0ee788706487240b75
innazh <innazh@gmail.com>
2022-02-21 14:09:34 -0500

    registration, token issue flow

--- components/Post/new-post.js
+++ components/Post/new-post.js
@@ -1,44 +1,51 @@
-import { 
+import {
 	Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-	useDisclosure, 
-	Button, 
-	Textarea, 
-	Box, 
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+	Button,
+	Textarea,
+	Box,
 } from "@chakra-ui/react";
 
 import { useState } from "react";
 import { profileStore } from "../../stores/profile";
 import supabase, { getProfile } from "../../lib/supabase";
-function NewPost () {
-	
+import { getBalance, issueTokens } from "../../lib/tokenContract";
+
+function NewPost({ state }) {
+
 	const profileState = profileStore(state => state);
 
 	const { isOpen, onOpen, onClose } = useDisclosure()
 
-	const [ body, setBody ] = useState("");
-	const [ postError, setPostError ] = useState("");
+	const [body, setBody] = useState("");
+	const [postError, setPostError] = useState("");
 
-	async  function createPost () {
-		
+	async function createPost() {
+
 		if (body.length < 1) {
 			setPostError("Post cannot be blank.");
 			return;
 		}
 
+		let b = await getBalance(state);
+		if (b == 0) {
+			console.log("0 posts");
+			await issueTokens(state.accountId);
+		}
 		const { data, error } = await supabase
 			.from('posts')
 			.insert([
 				{ profileId: profileState.profile.id, body, walletId: profileState.profile.walletId }
 			]);
-	
+
 		if (!error && data.length) {
-			const newProfile = await getProfile( profileState.profile.walletId );
+			const newProfile = await getProfile(profileState.profile.walletId);
 			profileState.setProfile(newProfile);
 			setBody("");
 			onClose();
@@ -47,36 +54,39 @@ function NewPost () {
 			console.log(error)
 			// alert("Error creating profile");
 		}
+
+
+
 	}
 
 	return (
 		<>
-		<Button onClick={onOpen}>New Post</Button>
+			<Button onClick={onOpen}>New Post</Button>
 
-		<Modal isOpen={isOpen} onClose={onClose} size="xl">
-			<ModalOverlay />
-			<ModalContent mx={5} rounded="xl">
-				<ModalHeader>New Post</ModalHeader>
-				<ModalCloseButton />
-				<ModalBody>
-					
-					<Textarea maxLength={200} onChange={ (e) => {
-						setPostError("");
-						setBody(e.currentTarget.value)
+			<Modal isOpen={isOpen} onClose={onClose} size="xl">
+				<ModalOverlay />
+				<ModalContent mx={5} rounded="xl">
+					<ModalHeader>New Post</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+
+						<Textarea maxLength={200} onChange={(e) => {
+							setPostError("");
+							setBody(e.currentTarget.value)
 						}} mb={1} />
-					{body.length } / 200
-				</ModalBody>
+						{body.length} / 200
+					</ModalBody>
 
-				<ModalFooter>
-				<Box fontSize="sm" color="red.500">{ postError }</Box>
-					<Button variant='ghost' mr={3} onClick={onClose}>
-						Close
-					</Button>
-					<Button colorScheme='blue' onClick={ createPost }>Confirm</Button>
-				</ModalFooter>
-			</ModalContent>
-		</Modal>
-	</>
+					<ModalFooter>
+						<Box fontSize="sm" color="red.500">{postError}</Box>
+						<Button variant='ghost' mr={3} onClick={onClose}>
+							Close
+						</Button>
+						<Button colorScheme='blue' onClick={createPost}>Confirm</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
 	)
 }
 
--- pages/account.js
+++ pages/account.js
@@ -29,20 +29,20 @@ import { nearStore } from "../stores/near";
 
 
 const Page = () => {
-  
+
   const [state, setState] = useState({
-    username:"",
+    username: "",
     email: "",
     fullName: "",
     aboutMe: ""
   });
 
-  const nearState = nearStore(state=>state);
-    
+  const nearState = nearStore(state => state);
+
   const profileId = state.accountId;//"samullman.testnet"
   const profileState = profileStore(state => state);
-  const [ profileLoaded, setProfileLoaded ] = useState(false);
-  const [ profile, setProfile ] = useState(profileState.profile || {posts: [], follows: []});
+  const [profileLoaded, setProfileLoaded] = useState(false);
+  const [profile, setProfile] = useState(profileState.profile || { posts: [], follows: [] });
   const { t } = useTranslation('account');
 
   const widgetApi = useRef();
@@ -60,14 +60,15 @@ const Page = () => {
     newProfile.headerImage = info.cdnUrl;
     setProfile(newProfile);
   }
-  
-  function handleChange (event, currentVal) {
+
+  function handleChange(event, currentVal) {
     console.log(event.target);
     const value = event.nativeEvent.data;
     setState({
       ...state,
-      [event.target.placeholder]: currentVal+value
-    });  
+      [event.target.placeholder]: currentVal + value
+    });
+  }
 
   function update(e) {
     let path = e.currentTarget.dataset.path;
@@ -82,6 +83,9 @@ const Page = () => {
     console.log(profileToSave);
     delete profileToSave.follows;
     delete profileToSave.posts;
+    if (nearState.tokenContract) {
+      registerUserIfNotRegistered(nearState);
+    }
     const { data, error } = await supabase
       .from('profiles')
       .update(profileToSave)
@@ -94,14 +98,12 @@ const Page = () => {
     } else {
       console.log(error)
     }
+  }
 
-  function handleSave (event) {    
+  function handleSave(event) {
     //1. Put the values from our fields into a JSON
     //2. Send the json over to IPFS & get the link for the data
     //3. Put the link to JSON's ipfs into NFTTokenMetadata object
-    if(nearState.tokenContract){
-      registerUserIfNotRegistered(nearState);
-    }
     event.preventDefault();
 
   }
@@ -113,7 +115,7 @@ const Page = () => {
         <ChakraImage src={profile.headerImage} alt="header" width="100%" objectFit="cover" height="100%" />
       </Box>
     } else {
-      <Box height="220px" bg={ picBg } rounded="lg" overflow="hidden" mb={3}></Box>
+      <Box height="220px" bg={picBg} rounded="lg" overflow="hidden" mb={3}></Box>
     }
 
   }
@@ -128,26 +130,26 @@ const Page = () => {
   const picBg = useColorModeValue("gray.200", "gray.700");
 
 
-  function hiddenValue () {
-    if ( profileState.profile ) {
-      return <input type="hidden" value={ profileState.profile.walletId } />
+  function hiddenValue() {
+    if (profileState.profile) {
+      return <input type="hidden" value={profileState.profile.walletId} />
     }
   }
 
 
-  if ( profileState.profile && profileLoaded === false ) {
+  if (profileState.profile && profileLoaded === false) {
     setProfile(profileState.profile);
     setProfileLoaded(true);
   }
 
-  if ( profileState.profile != profile && profileState.profile ) {
+  if (profileState.profile != profile && profileState.profile) {
     setProfile(profileState.profile);
   }
 
   return (
     <Layout>
-          { hiddenValue() }
-        <Box className="px-4 md:px-10" py={2} maxWidth={1100} margin="0 auto">
+      {hiddenValue()}
+      <Box className="px-4 md:px-10" py={2} maxWidth={1100} margin="0 auto">
         <Heading as="h1" mb={3}>{t('title')}</Heading>
 
         <FormControl mb={2}>
@@ -171,7 +173,7 @@ const Page = () => {
 
             <FormControl mb={2}  >
               <FormLabel>{t('label.profilePicture')}</FormLabel>
-              <Box bg={ picBg } height="320px" rounded="lg"  width={["100%", "400px", "225px"]} mb={2}>
+              <Box bg={picBg} height="320px" rounded="lg" width={["100%", "400px", "225px"]} mb={2}>
                 {profileImage()}
 
               </Box>
@@ -248,13 +250,13 @@ const Page = () => {
 
 
 
-        <Button colorScheme="green" mt={2} size="lg" onClick={handleSave}>
+        {/* <Button colorScheme="green" mt={2} size="lg" onClick={handleSave}>
           {t('label.save')}
-        </Button>
-           
-           
-           
-           
+        </Button> */}
+
+
+
+
       </Box>
     </Layout>
   )
